801d207b80cf965778029155d48ae66379b18b6d
whichlight <whichlight@gmail.com>
2024-01-29 18:04:39 -0800

    updating for mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Slider from 'react-input-slider';
-import './App.css'; // Adjust the path if your CSS file is in a different directory
-
+import React, { useState, useEffect } from "react";
+import Slider from "react-input-slider";
+import "./App.css"; // Adjust the path if your CSS file is in a different directory
 
 function App() {
   const [imageDescriptions, setImageDescriptions] = useState([]);
@@ -9,22 +8,29 @@ function App() {
   const [descriptionIndex, setDescriptionIndex] = useState(0);
 
   useEffect(() => {
-    fetch(process.env.PUBLIC_URL + '/images_descriptions.json')
+    fetch(process.env.PUBLIC_URL + "/images_descriptions.json")
       .then((response) => response.json())
       .then((data) => setImageDescriptions(data.image_descriptions))
-      .catch((error) => console.error("Error fetching image descriptions:", error));
+      .catch((error) =>
+        console.error("Error fetching image descriptions:", error)
+      );
   }, []);
 
   const handleChangeImage = (index) => {
     setSelectedImageIndex(index);
     setDescriptionIndex(0); // Reset description index when changing image
-  }
+  };
 
   // Get the current image's descriptions in the correct order
   const getDescription = (index) => {
     const description = imageDescriptions[index];
-    return [description.alt, description.short, description.description, description.detailed];
-  }
+    return [
+      description.alt,
+      description.short,
+      description.description,
+      description.detailed,
+    ];
+  };
 
   // If the JSON data hasn't been loaded yet, return a loading state
   if (imageDescriptions.length === 0) {
@@ -32,7 +38,7 @@ function App() {
   }
 
   function createMarkup(text) {
-    const html = text.replace(/\n/g, '<br/>');
+    const html = text.replace(/\n/g, "<br/>");
     return { __html: html };
   }
 
@@ -41,54 +47,55 @@ function App() {
   return (
     <div className="App">
       <h1>Image Description Zoom</h1>
-  
+
       <div className="controls">
-      <div>
-        {imageDescriptions.map((img, index) => (
-          <img
-            key={index}
-            src={img.src}
-            alt={img.alt}
-            onClick={() => handleChangeImage(index)}
-            style={{
-              border: selectedImageIndex === index ? '2px solid blue' : 'none',
-              cursor: 'pointer',
-              width: '100px',
-              height: '100px'
-            }}
-          />
-        ))}
-              </div>
-
-      <div className="slider-container">
-        <input
-          type="range"
-          min="0"
-          max="3"
-          value={descriptionIndex}
-          onChange={(e) => setDescriptionIndex(parseInt(e.target.value, 10))}
-        />
+        <div>
+          {imageDescriptions.map((img, index) => (
+            <img
+              key={index}
+              src={img.src}
+              alt={img.alt}
+              onClick={() => handleChangeImage(index)}
+              style={{
+                border:
+                  selectedImageIndex === index ? "2px solid blue" : "none",
+                cursor: "pointer",
+                width: "100px",
+                height: "100px",
+              }}
+            />
+          ))}
+        </div>
       </div>
-      </div>
-
-
 
       <div className="selected-content">
+        <div className="selected-image-container">
+          <img
+            className="selected-image"
+            src={imageDescriptions[selectedImageIndex].src}
+            alt={descriptions[0]} // Using the alt text for the alt attribute
+          />
+        </div>
 
-      <div className="selected-image-container">
-      <img
-        className="selected-image"
-        src={imageDescriptions[selectedImageIndex].src}
-        alt={descriptions[0]} // Using the alt text for the alt attribute
-      />
-    </div>
-
-      <div>
-     
-      </div>
-      <div className="description-text">
-      <div dangerouslySetInnerHTML={createMarkup(descriptions[descriptionIndex])}></div>
-      </div>
+        <div></div>
+        <div className="description-text">
+          <div className="slider-container">
+            <input
+              type="range"
+              min="0"
+              max="3"
+              value={descriptionIndex}
+              onChange={(e) =>
+                setDescriptionIndex(parseInt(e.target.value, 10))
+              }
+            />
+          </div>
+          <div
+            dangerouslySetInnerHTML={createMarkup(
+              descriptions[descriptionIndex]
+            )}
+          ></div>
+        </div>
       </div>
     </div>
   );
